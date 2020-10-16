ff5c2abb2f453d110ed0e7bff19130bf0515e7ca
Patricia Selpa <patricia.selpa@gmai.com>
2020-10-16 15:10:38 -0300

    Criada a classe Championship

---
+++ Domain/Championship.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Championship
+    {
+        public int DisputedMatches {get; set;}
+        public int TeamPoints {get; set;}
+        public int TeamVictories {get; set;}
+        public int TeamDefeats {get; set;}
+        public int Ties {get; set;}
+        public int GoalsOutcome {get; set;}
+        public int ProperGoals {get; set;}
+        public int OwnGoals {get; set;}
+        public double EfficiencyPercent {get; set;}
+        public List<Player> TopScorer {get; set;}
+        public List<Team> RelegatedTeam {get; set;}
+        public List<Team> QualifiedTeam {get; set;}
+
+
+        public void CreateDispute()
+        {
+
+        }
+
+        public void ShowResults()
+        {
+
+        }
+
+        public void ShowTeamPoints()
+        {
+
+        }
+    }
+}
