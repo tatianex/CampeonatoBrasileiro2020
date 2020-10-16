a48a26b8def4a2cbdad233a8620c81c88132fc9b
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-16 13:29:57 -0300

    adicionada a classe Team

---
+++ Domain/Team.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Domain
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+
+        public AddPlayer()
+        {
+
+        }
+        public RemovePlayer()
+        {
+            
+        }
+    }
+}
