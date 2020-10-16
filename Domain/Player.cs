49c2a75afa5db4f177a03980f0f0af2212a26a80
Patricia Selpa <patricia.selpa@gmai.com>
2020-10-16 14:06:04 -0300

    Criada a classe Player.

---
+++ Domain/Player.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Domain
+{
+    public class Player
+    {
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+        public string Goal {get; set;}
+        public string OwnGoal {get; set;}
+
+    }
+}
