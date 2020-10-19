c877068315fb8c360d585827f3a18cfd892bd1bc
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-19 19:02:24 -0300

    Criado método CreateTeams

--- Domain/Team.cs
+++ Domain/Team.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 namespace Domain
 {
     public class Team
-    {
+    {  
         public string Id { get; set; }
         public string Name { get; set; }
 
-        public Team(string id, string name, User user)
+        public Team()
         {
-            if (user.Profile == User.UserProfile.CBF)
+            // Lista privada para acesso do torcedor
+            playersList = new List<Player>();
+            
+        }
+        private List<Player> playersList { get; set; }
+        public IReadOnlyCollection<Player> Players => playersList;
+
+        public bool CreateTeams(List<Player> players, string password)
+        {
+            if (password == "459iMs@;3eZ!8*")
             {
-                Id = id;
-                Name = name;
+                playersList = players;
+                return true;
             }
+            return false;
         }
 
         public void AddPlayer(Player player)
