f526460b31e77f1445be020dff928f66461a7e7b
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-20 15:29:54 -0300

    Criando método para criar rodadas

--- Domain/Championship.cs
+++ Domain/Championship.cs
@@ -5,7 +5,6 @@ namespace Domain
 {
     public class Championship
     {
-        public int TotalRound { get; set; } = 7;
         private List<Team> Teams { get; set; }
         private List<SoccerRound> _rounds { get; set; }
         public IReadOnlyCollection<SoccerRound> Rounds => _rounds;
@@ -22,12 +21,9 @@ namespace Domain
 
         private void CreateRounds(List<Team> teams, User user)
         {
-            if ((user.Profile == User.UserProfile.CBF) && (teams.Count > 7) && (teams.Count % 2 == 0))
+            for (int round = 0; round < teams.Count - 1; round++)
             {
-                //Criar automaticamente uma lista tuplas onde cada time joga com os outros 1 vez.
-
-                _rounds.Add(new SoccerRound(teams, user));
-
+                _rounds.Add(new SoccerRound(round, teams, user));
             }
         }
 
--- Domain/SoccerRound.cs
+++ Domain/SoccerRound.cs
@@ -8,31 +8,51 @@ namespace Domain
         public int RoundNumber { get; set; }
         public DateTime BeginingDate { get; set; }
         public DateTime EndDate { get; set; }
-        public List<(Team, Team)> RoundGames { get; set;}
+        public List<(int, Team, Team)> RoundGames { get; set;}
 
-        public SoccerRound(List<Team> teams, User user) 
+        public SoccerRound(int round, List<Team> teams, User user) 
         {
-            var sizeOfRoundGroup = teams.Count / 2;
-
-            for (int i = 0; i <= sizeOfRoundGroup; i++)
+            if ((user.Profile == User.UserProfile.CBF) && (teams.Count > 7) && (teams.Count % 2 == 0))
             {
-                RoundGames.Add((teams[i], teams[teams.Count - i]));
+                var sizeOfRoundGroup = teams.Count / 2;
+                var totalRounds = teams.Count - 1;
+                var actualRound = 1;
+                
+                for (int team1 = 0; team1 <= totalRounds; team1++)
+                {                  
+                    for (int team2 = totalRounds; team2 >= 0; team2--)
+                    {
+                        if (teams[team1].Id != teams[team2].Id)
+                        {
+                            RoundGames.Add((actualRound, teams[team1], teams[team2]));
+                            break;
+                        }
+                    }
+                    actualRound++;
+                }
             }
 
             /*
-                Fluminense
-                Flamengo
-                Santos
-                Vasco 
-                Botafogo
-                Palmeiras
-                Cruzeiro
-                Atlético Mineiro
+                A rodada é formada selecionando a lista de times, criando uma nova lista
+                onde são passados o primeiro time da lista e o último para formar um jogo
+                com 4 jogos (considerando que tenham 8 times) forma-se uma rodada.
 
-                Fluminense x Atlético Mineiro
-                Flamengo x Cruzeiro
-                Santos x Palmeiras
-                Vasco x Botafogo
+                0 Fluminense
+                1 Flamengo
+                2 Santos
+                3 Vasco 
+                4 Botafogo
+                5 Palmeiras
+                6 Cruzeiro
+                7 Atlético Mineiro
+
+                Rodada 1 - Jogo 1: Fluminense x Atlético Mineiro
+                Rodada 1 - Jogo 2: Fluminense x Cruzeiro
+                Rodada 1 - Jogo 3: Fluminense x Palmeiras
+                Rodada 1 - Jogo 4: Fluminense x Botafogo
+
+                Rodada 2 - Jogo 5: Flamengo x Atlético Mineiro
+                Rodada 2 - Jogo 6: Fl
             */
         }
 
