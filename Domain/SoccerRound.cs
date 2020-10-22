7eb6d9e13dbced34cf6f13814bbc716218089629
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-22 19:54:08 -0300

    Trabalhando no método de criar rodadas e tentando implementar os testes

--- Domain/SoccerRound.cs
+++ Domain/SoccerRound.cs
@@ -8,7 +8,7 @@ namespace Domain
         public int RoundNumber { get; set; }
         public DateTime BeginingDate { get; set; }
         public DateTime EndDate { get; set; }
-        public List<(int, Team, Team)> RoundGames { get; set;}
+        public List<(Team, Team)> RoundGames { get; set;}
 
         public SoccerRound(int round, List<Team> teams, User user) 
         {
@@ -18,22 +18,21 @@ namespace Domain
                 && (teams.Count % 2 == 0)
             )
             {
-                var sizeOfRoundGroup = teams.Count / 2;
+                var amountOfGamesPerRound = teams.Count / 2;
                 var totalRounds = teams.Count - 1;
                 var actualRound = 1;
 
-                var games = teams.Count -1 * 4;
+                var games = teams.Count -1 * amountOfGamesPerRound;
                 var sortition = new Random();
                 sortition.Next(games);
 
-                
                 for (int team1 = 0; team1 <= totalRounds; team1++)
                 {                  
                     for (int team2 = totalRounds; team2 >= 0; team2--)
                     {
                         if (teams[team1].Id != teams[team2].Id)
                         {
-                            RoundGames.Add((actualRound, teams[team1], teams[team2]));
+                            RoundGames.Add((teams[team1], teams[team2]));
                             break;
                         }
                     }
@@ -46,14 +45,7 @@ namespace Domain
                 onde são passados o primeiro time da lista e o último para formar um jogo
                 com 4 jogos (considerando que tenham 8 times) forma-se uma rodada.
 
-                0 Fluminense
-                1 Flamengo
-                2 Santos
-                3 Vasco 
-                4 Botafogo
-                5 Palmeiras
-                6 Cruzeiro
-                7 Atlético Mineiro
+                {Fluminense, Flamengo, Santos, Vasco, Botafogo, Palmeiras, Cruzeiro, Atlético Mineiro}
 
                 Rodada 1 - Jogo 1: Fluminense x Atlético Mineiro
                 Rodada 1 - Jogo 2: Fluminense x Cruzeiro
--- Test/TeamTest.cs
+++ Test/TeamTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using Domain;
 
 namespace Test
 {
@@ -8,7 +10,17 @@ namespace Test
         [Fact]
         public void should_do_something()
         {
-        
+            var teams = new List<Team>();
+            
+                /* Guid new Guid(), "Palmeiras",
+                Guid new Guid(), "Vasco",
+                Guid new Guid(), "Fluminense",
+                Guid new Guid(), "Flamengo",
+                Guid new Guid(), "São Paulo",
+                Guid new Guid(), "Corínthias",
+                Guid new Guid(), "Botafogo",
+                Guid new Guid(), "Grêmio" */
+            
         }
     }
 }
